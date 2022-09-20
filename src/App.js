89b5813c94c5070cd2eb6778d92e68ba29422e95
akifbsr <akifberabasar@gmail.com>
2022-09-20 10:34:10 +0300

    react ders 11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let arr = [
+    {   id:1,
+        title:"Dağ 1",
+        par: "Açıklama 1"
+    },
+    { 
+        id:2,
+        title:"Dağ 2",
+        par: "Açıklama 2"
+    }, {
+        id:3, 
+        title:"Dağ 3",
+        par: "Açıklama 3"
+    },
+];
+
+
+
+
+const App = () => { 
+  const [lesson, setLesson] = useState(11);
+   
+   return <div>
+      <h1>Başlık</h1>
+      <h2>Ders {lesson}</h2>
+      <button onClick={() => {
+        setLesson(lesson + 1);
+      }}
+      
+      >Ders Arttır</button>
+
+      <button onClick={() => {
+        setLesson(lesson - 1);
+      }}
+      
+      >Ders Azalt </button>
+
+      <p>Başlangıç</p>
+      <div className="Cards">
+        {arr.map(({title, par} , i)  => (
+         <Card 
+         key={`index ${i}`}
+         title={title}
+         par={par}  
+         lesson={lesson} 
+         index={i}
+         
+         />
+
+        ))}
+     </div>
+    </div>;
+    
+  
 }
 
 export default App;
